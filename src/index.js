7771a94672b8561756212c2c3c1582ecdc0699ce
youknow-me <156052413+youknow-me@users.noreply.github.com>
2026-05-25 09:07:43 +0530

    Fixed wildcard route for express v5

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
-app.get("*", (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
